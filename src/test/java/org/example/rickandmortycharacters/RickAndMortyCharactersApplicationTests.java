a94f96321fec229b34d7e4c344dadf3161c0d851
Pourya <pourya.nejadensan@gmail.com>
2024-07-23 15:21:56 +0200

    Initial commit

---
+++ src/test/java/org/example/rickandmortycharacters/RickAndMortyCharactersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rickandmortycharacters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickAndMortyCharactersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
